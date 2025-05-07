45c9dc0c5ca58bd8d81fe4a87c71ac414c94f880
Filip <filipwegrzyn85@gmail.com>
2025-05-07 23:26:56 +0200

    initial setup

---
+++ src/main/java/dev/fwegrzyn/charity_fund_manager/CharityFundManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.fwegrzyn.charity_fund_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityFundManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityFundManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fwegrzyn/charity_fund_manager/CharityFundManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fwegrzyn.charity_fund_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityFundManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
